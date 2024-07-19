e2bfcfd59f7479c0cfc24802362a67ba982c4a81
Julián <informacionlinux@gmail.com>
2024-07-18 22:06:15 -0700

    config 1 notifictinos

---
+++ components/Sales.js
@@ -0,0 +1,10 @@
+import  React, {}  from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+const Sales = () => {
+ return (
+    <Text>Bienvenido</Text>
+ )
+}
+
+export default Sales
