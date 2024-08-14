c2478dc81d3eb1b510d3ba751f49c457eb66fbca
Julian <informacionlinux271087@gmail.com>
2024-08-13 20:35:02 -0700

    Primera actualizacion con la primera libreria  native  notify

--- App.js
+++ App.js
@@ -9,48 +9,18 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider } from '@ui-kitten/components';
 import * as Notifications from 'expo-notifications';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import registerNNPushToken from 'native-notify';
 
 export default function App() {
-  useEffect(() => {
-    // Request permissions and get a token
-    const requestPermissions = async () => {
-      try {
-        const { status } = await Notifications.requestPermissionsAsync();
-        if (status !== 'granted') {
-          console.error('Failed to get push token for push notification!');
-          return;
-        }
-        const token = (await Notifications.getExpoPushTokenAsync()).data;
-        await AsyncStorage.setItem("NOTIFY-TK", token);
-        console.log('Expo Push Token:', token);
-      } catch (error) {
-        console.error('Error requesting permissions or getting token:', error);
-      }
-    };
-    requestPermissions();
+  
+  registerNNPushToken(23061, 'op0fHTEFY2CT43w3D4WvXA');
+  
 
-    // Handle foreground notifications
-    const foregroundSubscription = Notifications.addNotificationReceivedListener(notification => {
-      console.log('Notification received in foreground:', notification);
-      // Puedes agregar lógica aquí para mostrar un mensaje o actualizar el estado
-    });
 
-    // Handle notification clicks
-    const responseSubscription = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log('Notification clicked:', response);
-      const data = response.notification.request.content.data;
-      if (data && data.screen) {
-        // navigation.navigate(data.screen); // Asegúrate de tener acceso a la función de navegación
-      }
-    });
-
-    return () => {
-      foregroundSubscription.remove();
-      responseSubscription.remove();
-    };
-  }, []);
 
   return (
+
+
     <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar style="auto" />
       <ApplicationProvider {...eva} theme={eva.light}>
